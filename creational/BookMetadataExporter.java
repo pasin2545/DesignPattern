da0b22ddda29a16226110537257b84ee241d40d8
-Nammyo- <69385808+pasin2545@users.noreply.github.com>
2022-10-25 23:37:27 +0700

    Add files via upload

---
+++ creational/BookMetadataExporter.java
@@ -0,0 +1,18 @@
+package creational;
+
+import java.io.PrintStream;
+
+public abstract class BookMetadataExporter extends BookCollection {
+    
+	public abstract BookMetadataFormatter createFormatter();
+	
+	public void export(PrintStream stream) {
+		BookMetadataFormatter exporter = createFormatter();
+		if (exporter != null) {
+			for (Book i : books) {
+				exporter.append(i);
+			}
+			stream.println(exporter.getMetadataString());
+		}
+	}
+}
